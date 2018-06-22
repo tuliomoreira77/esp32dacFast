a60aa8345c0adcf0d7ce72f0864980ab01a1b14d
tuliomoreira77 <39540679+tuliomoreira77@users.noreply.github.com>
2018-06-22 18:20:09 -0300

    Add files via upload

---
+++ src/esp32dacFast.cpp
@@ -0,0 +1,44 @@
+#include "esp32dacFast.h"
+#include "esp32-hal-dac.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "rom/ets_sys.h"
+#include "esp_attr.h"
+#include "esp_intr.h"
+#include "soc/rtc_io_reg.h"
+#include "soc/rtc_cntl_reg.h"
+#include "soc/sens_reg.h"
+
+void dacAttachPin(uint8_t pin)
+{
+  if(pin < 25 || pin > 26)
+  {
+    return;
+  }
+  pinMode(pin, ANALOG);
+  
+  CLEAR_PERI_REG_MASK(SENS_SAR_DAC_CTRL1_REG, SENS_SW_TONE_EN);
+  if(pin == 25)
+  {
+    CLEAR_PERI_REG_MASK(SENS_SAR_DAC_CTRL2_REG, SENS_DAC_CW_EN1_M);
+    SET_PERI_REG_BITS(RTC_IO_PAD_DAC1_REG, RTC_IO_PDAC1_DAC, 0, RTC_IO_PDAC1_DAC_S);
+    SET_PERI_REG_MASK(RTC_IO_PAD_DAC1_REG, RTC_IO_PDAC1_XPD_DAC | RTC_IO_PDAC1_DAC_XPD_FORCE);
+  }
+  else
+  {
+    CLEAR_PERI_REG_MASK(SENS_SAR_DAC_CTRL2_REG, SENS_DAC_CW_EN2_M);
+    SET_PERI_REG_BITS(RTC_IO_PAD_DAC2_REG, RTC_IO_PDAC2_DAC, 0, RTC_IO_PDAC2_DAC_S);
+    SET_PERI_REG_MASK(RTC_IO_PAD_DAC2_REG, RTC_IO_PDAC2_XPD_DAC | RTC_IO_PDAC2_DAC_XPD_FORCE);
+  }
+}
+
+void dacFastWrite25(uint8_t value)
+{
+  SET_PERI_REG_BITS(RTC_IO_PAD_DAC1_REG, RTC_IO_PDAC1_DAC, value, RTC_IO_PDAC1_DAC_S);
+}
+
+void dacFastWrite26(uint8_t value)
+{
+  SET_PERI_REG_BITS(RTC_IO_PAD_DAC2_REG, RTC_IO_PDAC2_DAC, value, RTC_IO_PDAC2_DAC_S);
+}
+
---
+++ src/esp32dacFast.h
@@ -0,0 +1,11 @@
+#ifndef esp32dacFast_h
+#define esp32dacFast_h
+#include "esp32-hal.h"
+#include "driver/gpio.h"
+
+
+
+void dacAttachPin(uint8_t pin);
+void dacFastWrite25(uint8_t value);
+void dacFastWrite26(uint8_t value);
+#endif
